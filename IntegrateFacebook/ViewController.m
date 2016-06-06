69157797c50dbb694ef81ee9ec0250341b254d24
Gopal Vaid <gopalvaid@ravis-mac-mini.local>
2016-06-06 12:52:03 +0530

    login facebook in xcode 7.3

--- IntegrateFacebook/ViewController.m
+++ IntegrateFacebook/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
 
 @interface ViewController ()
 
@@ -17,6 +19,76 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+   // FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+   // loginButton.center = self.view.center;
+  //  [self.view addSubview:loginButton];
+    
+    
+    // Add a custom login button to your app
+    UIButton *myLoginButton=[UIButton buttonWithType:UIButtonTypeCustom];
+    myLoginButton.backgroundColor=[UIColor darkGrayColor];
+    myLoginButton.frame=CGRectMake(0,0,180,40);
+    myLoginButton.center = self.view.center;
+    [myLoginButton setTitle: @"My Login Button" forState: UIControlStateNormal];
+    
+    // Handle clicks on the button
+    [myLoginButton
+     addTarget:self
+     action:@selector(loginButtonClicked) forControlEvents:UIControlEventTouchUpInside];
+    
+    // Add the button to the view
+    [self.view addSubview:myLoginButton];
+}
+
+// Once the button is clicked, show the login dialog
+-(void)loginButtonClicked
+{
+    
+//    if ([[FBSDKAccessToken currentAccessToken] hasGranted:@"publish_actions"]) {
+//        // TODO: publish content.
+//    } else {
+    FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
+    [login
+     logInWithReadPermissions: @[@"public_profile", @"email", @"user_friends"]
+     fromViewController:self
+     handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
+         if (error) {
+             NSLog(@"Process error");
+         } else if (result.isCancelled) {
+             NSLog(@"Cancelled");
+         } else {
+             if ([result.grantedPermissions containsObject:@"email"])
+             {
+                 NSLog(@"result is:%@",result);
+                 [self fetchUserInfo];
+             }
+         }
+     }];
+   // }
+}
+
+
+-(void)fetchUserInfo
+{
+    if ([FBSDKAccessToken currentAccessToken])
+    {
+        NSLog(@"Token is available : %@",[[FBSDKAccessToken currentAccessToken]tokenString]);
+        
+        [[[FBSDKGraphRequest alloc] initWithGraphPath:@"me" parameters:@{@"fields": @"id, name, link, first_name, last_name, picture.type(large), email, birthday, bio ,location ,friends ,hometown , friendlists"}]
+         startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
+             if (!error)
+             {
+                 NSLog(@"resultis:%@",result);
+             }
+             else
+             {
+                 NSLog(@"Error %@",error);
+             }
+         }];
+        
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
